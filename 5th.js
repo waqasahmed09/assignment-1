d2f92fd1f5877bb34cbcca9a14f59a940edad104
waqas <waqas.se2020@gmail.com>
2023-10-25 19:01:39 +0500

    thora kam kyaa

---
+++ 5th.js
@@ -0,0 +1,31 @@
+
+    let name = prompt("What's your name");
+
+    alert(`hi, ${name}`);
+
+    //Multiplication of Table 
+
+    alert(` 7 x 1 = 7
+    7 x 2 = 14
+    7 x 3 = 21
+    7 x 4 = 28
+    7 x 5 = 35
+    7 x 6 = 42
+    7 x 7 = 49
+    7 x 8 = 56
+    7 x 9 = 63
+    7 x 10 = 70 `)
+
+
+    let cityName = prompt("Enter your City name")
+
+    if (cityName === "Karachi") {
+       console.log("WElcome in my City") 
+    } else {
+        console.log("bye bye")
+    }
+
+
+    //Question No. 4
+
+    
